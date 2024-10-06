40da8dadb78ed9c282d58c76672a51d990fbdc6b
madhavanrx18 <126698811+madhavanrx18@users.noreply.github.com>
2024-10-06 13:59:43 +0000

    work

--- Secure_coding/lab2/arr01.c
+++ Secure_coding/lab2/arr01.c
@@ -1,16 +1,22 @@
-void wclear(int arr[]){
-    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
-        arr[i] = 0;
-    }
-}
-void rclear(int arr[],int size){
-    for(int i = 0; i < size; i++){
-        arr[i] = 0;
-    }
+#include <stdio.h>
+
+int main()
+{
+    int s1,s2,s3, a[5] = {10,20,30,40,50};
+    s1 = sizeof(a)/sizeof(a[0]);
+    s2 = arr_sizeVul(a);
+    s3 = arr_size(a,s1);
+    printf("Size of arr is : %d\n",s1);
+    printf("Size of arr in the vulnerable function is : %d\n",s2);
+    printf("Size of arr in the non vulnerable function is : %d\n",s3);
 }
 
-void main(){
-    int arr[5];
-    wclear(arr);
-    rclear(arr,sizeof(arr)/sizeof(arr[0])); 
+int arr_sizeVul(int arr[]){
+    int s = sizeof(arr)/sizeof(arr[0]);
+    return s;
+}
+
+int arr_size(int arr[],int size){
+    int s = size;
+    return s;
 }
